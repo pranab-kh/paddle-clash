b620b320d12649b98316c6360897c54531bb805f
SakarDallakoti <081bct069.sakar@pcampus.edu.np>
2026-03-23 17:28:51 +0545

    try again

--- include/graphicslibrary.h
+++ include/graphicslibrary.h
@@ -181,11 +181,7 @@ struct Paddle: public Ellipsoid{
         updateAllPoints();
     }
 
-    void updateHitboxes(){
-    // Z axis is reversed so z has negative sign
-    hitBoxMin = Vec3(pos.x - radius, pos.y - radius, pos.z + 0.05f);
-    hitBoxMax = Vec3(pos.x + radius, pos.y + radius, pos.z - 0.05f);
-    }
+    
 
 };
 
