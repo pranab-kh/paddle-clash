2c3ded840517616c9f19701724b505ca0000a99c
Rikison Rijal <rikisonrijal000@gmail.com>
2026-03-23 11:46:53 +0000

    Finally resolved

--- include/graphicslibrary.h
+++ include/graphicslibrary.h
@@ -6,7 +6,6 @@
 #include "vaovbo.h"
 #include "math3d.h"
 
-
 // RGB struct to convert RGB colors to normalized form
 struct rgb
 {
@@ -46,8 +45,6 @@ struct Ellipsoid{
             for(int j = 0; j < slices; j++)
             {
                 points[idx++] = pos.x + a * cosf(theta) * sinf(phi);
-                // points[idx++] = pos.y + b * sinf(theta) * sinf(phi);
-                // We are in (x,y,-z) octant so, z is subtracted
                 points[idx++] = pos.y + b * cosf(phi);
                 points[idx++] = pos.z - c * sinf(theta) * sinf(phi);
                 theta += thetastep;
@@ -62,22 +59,18 @@ struct Ellipsoid{
         updateAllPoints();
     }
 
-    void changeCenterCoords(float x, float y){
-        pos = Vec3(x, y);
+    void changeCenterCoords(float x, float y, float z){
+        pos = Vec3(x, y, z);
     }
 
     void incrementCenterCoords(float x, float y){
         pos = pos + Vec3(x, y);
     }
 
-
     ~Ellipsoid(){
         delete[] points;
     }  
 };
-
-
-
 
 // Paddle contains an ellipse partitioned into red and skin color in 70 : 30 ratio with a handle.
 struct Paddle: public Ellipsoid{
@@ -93,10 +86,8 @@ struct Paddle: public Ellipsoid{
     Vec3 hitBoxMax;
 
     Paddle(GLfloat a, GLfloat b, GLfloat c, GLfloat h = 0, GLfloat k = 0, GLfloat l = 0) : Ellipsoid(a, b, c, h, k, l){
-            // Hard coded the center position of the player racket in coordinates
             mousePrevPos = Vec3(0, 0.01, 3.5);
             mouseCurrentPos = Vec3(0, 0.01, 3.5);
-            // We only need to compare between two radii because 2 out of 3 radii are same and one 1 out of 3 is 0.
             radius = (a > b)? a : b;
             updateHandleCoords();
             updateHitboxes();
@@ -107,15 +98,11 @@ struct Paddle: public Ellipsoid{
              int idx = 0;
             // Left top edge
             for(int i = 0; i < 3; i++)
-            {
                 handleVertices[idx++] = points[handleStartIdx + i];
-            }
 
             // Right top edge
             for(int i = 0; i < 3; i++)
-            {
                 handleVertices[idx++] = points[handleEndIdx + i];
-            }
 
             // Left bottom edge
             handleVertices[idx++] = handleVertices[0];
@@ -134,9 +121,7 @@ struct Paddle: public Ellipsoid{
 
             // Right top edge
             for(int i = 0; i < 3; i++)
-            {
                 handleVertices[idx++] = points[handleEndIdx + i];
-            }
     }
 
     void updateAllPoints(){
@@ -146,76 +131,97 @@ struct Paddle: public Ellipsoid{
     }
 
     void movePaddle(float winWidth, float winHeight){
-        // Calculate the displacement
         Vec3 displacement = (mouseCurrentPos - mousePrevPos);
-        if(magnitude(displacement) == 0)
-        {
-            return;
-        }
-        // Normalize the displacement
+        if(magnitude(displacement) == 0) return;
+
         Vec3 normalizedDisplacement = normalize(displacement);
-        // In order to change the increase the speed of racket motion when mouse travels more distance, we use two scales
         float xScale = abs(displacement.x * 6.5)/(winWidth);
         float yScale = abs(displacement.y * 6.5)/(winHeight);
         normalizedDisplacement.x *= xScale;
         normalizedDisplacement.y *= yScale;
-
+        
         incrementCenterCoords(normalizedDisplacement.x, normalizedDisplacement.y);
-        // All points are updated according to the center that we just changed
 
-        // clamp within table boundaries
+        // Clamping within table boundaries (Merged from AI branch)
         if(pos.x < -2.5f) pos.x = -2.5f;  // left wall
         if(pos.x >  2.5f) pos.x =  2.5f;  // right wall
         if(pos.z < 0.1f)  pos.z = 0.1f;   // can't cross net
         if(pos.z >  4.8f) pos.z =  4.8f;  // can't go past near edge
 
-        updateAllPoints();
+        updateHitboxes(); // Crucial for collision detection!
     }
 
     void updateHitboxes(){
-    // Z axis is reversed so z has negative sign
-    hitBoxMin = Vec3(pos.x - radius, pos.y - radius, pos.z + 0.05f);
-    hitBoxMax = Vec3(pos.x + radius, pos.y + radius, pos.z - 0.05f);
+        // Hitbox logic from working branch
+        hitBoxMin = Vec3(pos.x - radius, pos.y-radius, pos.z - 0.05f);
+        hitBoxMax = Vec3(pos.x + radius, pos.y+radius, pos.z + 0.05f);
     }
-
 };
-
 
 struct Ball : public Ellipsoid{
-
-    const float coeffOfRestitution = 0.9;
+    float radius;
+    const float coeffOfRestitutionForTable = 0.9;
+    const float coeffOfRestitutionForPaddle = 2.0;
+    
     Ball(float rad, float h = 0, float k = 0, float l = 0) : Ellipsoid(rad, rad, rad, h, k, l){
-        acc = Vec3(0, -9.8/3, 0);
+        radius = rad;
+        acc = Vec3(0, -9.8, 3);
     }
 
-    // void updateKinematics(float deltaTime, Paddle& playerPaddle, Paddle& opponentPaddle)
-    void updateKinematics(float deltaTime)
+    void updateKinematics(float deltaTime, Paddle& playerPaddle, Paddle& opponentPaddle)
     {
+        acc.x *= 0.9;
+        acc.z *= 0.9;
         vel = vel + acc * deltaTime;
         pos = pos + vel * deltaTime;
-        // The ball bounces back if it hits the table
+        
+        // Bounce off table
         if(pos.y <= 0.01)
         {
-            vel.y *= -1 * coeffOfRestitution;
+            pos.y = 0.01;
+            vel.y *= -1 * coeffOfRestitutionForTable;
         }
 
-        // // From here
+        // Collision with player paddle
+         Vec3 closestPoint = getClosestPoint(playerPaddle.hitBoxMin, playerPaddle.hitBoxMax);
+         float dist = distance(pos, closestPoint);
+         if(dist <= radius)
+         {
+            vel.z *= -1 * coeffOfRestitutionForPaddle;
+            vel.y += -acc.y/2;
+         }
 
-        // // Collision with the paddles
+         // Collision with opponent paddle
+         closestPoint = getClosestPoint(opponentPaddle.hitBoxMin, opponentPaddle.hitBoxMax);
+         dist = distance(pos, closestPoint);
+         if(dist <= radius)
+         {
+            vel.z *= -1 * coeffOfRestitutionForPaddle;
+            vel.y += -acc.y/2;
+         }
+    }
 
-        // // Collision with the player paddle
-        // // Finding the  point in the hitbox of the paddle which is closest to the ball
-        //  float xClosest, yClosest, zClosest;
-        //  if(pos.x < playerPaddle.hitBoxMin.x)
-        //     xClosest = playerPaddle.hitBoxMin.x;
-        //  if(pos.x > xClosest)
+    Vec3 getClosestPoint(Vec3 hitBoxMin, Vec3 hitBoxMax)
+    {
+        Vec3 closest;
 
+        if(pos.x < hitBoxMin.x) closest.x = hitBoxMin.x;
+        else if(pos.x > hitBoxMax.x) closest.x = hitBoxMax.x;
+        else closest.x = pos.x;
 
-        updateAllPoints();
+        if(pos.y < hitBoxMin.y) closest.y = hitBoxMin.y;
+        else if(pos.y > hitBoxMax.y) closest.y = hitBoxMax.y;
+        else closest.y = pos.y;
+
+        if(pos.z < hitBoxMin.z) closest.z = hitBoxMin.z;
+        else if(pos.z > hitBoxMax.z) closest.z = hitBoxMax.z;
+        else closest.z = pos.z;
+
+        return closest;
     }
 };
 
-
+// AI Logic (Merged from broken branch, updated for GPU workflow)
 void updateAI(Paddle& aiPaddle, const Ball& ball, float deltaTime){
     float aiSpeed = 2.5f;
     float deadzone = 0.05f;
@@ -225,11 +231,12 @@ void updateAI(Paddle& aiPaddle, const Ball& ball, float deltaTime){
     else if(ball.pos.x < aiPaddle.pos.x - deadzone)
         aiPaddle.pos.x -= aiSpeed * deltaTime;
 
+    // AI boundaries
     if(aiPaddle.pos.x < -2.5f) aiPaddle.pos.x = -2.5f;
     if(aiPaddle.pos.x >  2.5f) aiPaddle.pos.x =  2.5f;
 
-    aiPaddle.updateAllPoints();
+    // Update the hitboxes so the ball can actually collide with the AI paddle!
+    aiPaddle.updateHitboxes();
 }
-
 
 #endif
